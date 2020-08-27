fe5d1930c0c47efc93b9249d290c45d17c33cf52
Hendrik <hvossi92@gmail.com>
2020-08-27 07:16:03 +0200

    First commit. Before uploading to github

---
+++ src/main/java/com/backend/LoginServlet.java
@@ -0,0 +1,16 @@
+package com.backend;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(urlPatterns = "/login")
+public class LoginServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("/WEB-INF/views/login.jsp").forward(request, response);
+    }
+}
---
+++ src/main/webapp/WEB-INF/views/login.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: vossi
+  Date: 8/27/20
+  Time: 7:00 AM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Login JSP</title>
+</head>
+<body>
+<p> Yo welcome to the world of logins!</p>
+</body>
+</html>
